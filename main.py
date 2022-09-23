8867d623e0debd08a5ef6fa5ea4e4beb3d06aaea
PrasitSahu <prasitsahu4@gmail.com>
2022-09-23 12:58:12 +0530

    minute changes

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ cls()
 
 
 # handles stop
-def stop():
-    if not car['is_running']:
+def stop(is_running):
+    if not is_running:
         print_error('The car is already stop.')
         return
 
@@ -22,13 +22,12 @@ def stop():
          `-(_)--(_)-'
 
     ''')
-    car['is_running'] = False
     print('\tBooyay! The car stopped.')
 
 
 # handles start
-def start():
-    if car['is_running']:
+def start(is_running):
+    if is_running:
         print_error('The car is already running.')
         return
 
@@ -39,7 +38,6 @@ def start():
       ===`-(_)--(_)-'
 
     ''')
-    car['is_running'] = True
     print('\tBooyay! The car started...')
 
 
@@ -50,13 +48,44 @@ def print_error(err):
 
 # main function
 def main():
+    print('''
+                            ______  __   __   ______  ______   _____   _   _   _____  ______ 
+                            | ___ \ \ \ / /   |  _  \ | ___ \ |_   _| | | | | |  ___| | ___ \\
+                            | |_/ /  \ V /    | | | | | |_/ /   | |   | | | | | |__   | |_/ /
+                            |  __/    \ /     | | | | |    /    | |   | | | | |  __|  |    / 
+                            | |       | |_    | |/ /  | |\ \   _| |_  \ \_/ / | |___  | |\ \ 
+                            \_|       \_(_)   |___/   \_| \_|  \___/   \___/  \____/  \_| \_|
+
+
+                                                     ▐▀▀▀▀▀▀▀▀▀▀▀▌
+                                                   ▄▄▌░░░░░░░░░░░▐▄▄
+                                                  ▄▄██▄▄▄▄▄▄▄▄▄▄▄██▄▄
+                                                 ▐█▄█┼┼█▓█▓█▓█▓█┼┼█▄█▌
+                                                ─█▓▓█▀███_6969_███▀█▓▓─
+                                                ─█▓▓█▀▀───▀▀▀▀──▀▀█▓▓█─
+                                                ─█▓▓█─────────────█▓▓█─
+
+    ''')
+
+    print('''
+
+            INSTRUCTIONS
+            ────────────
+            Type 'start' to start the car.
+            Type 'stop' to stop the car.
+            Type 'quit' to quit.
+
+    ''')
+    is_running = False
     while True:
         cmd = input('\n\t> ')
         cmd = cmd.lower().strip()
         if cmd == 'start':
-            start()
+            start(is_running)
+            is_running = True
         elif cmd == 'stop':
-            stop()
+            stop(is_running)
+            is_running = False
         elif cmd == 'quit':
             print('\n\tBye! Bye!\n')
             break
@@ -64,37 +93,5 @@ def main():
             print('\tInvalid Command. Try again...')
 
 
-print('''
-                         _____   _   _   _____   ______  ______   _____   _   _   _____  ______ 
-                        |_   _| | | | | |  ___|  |  _  \ | ___ \ |_   _| | | | | |  ___| | ___ \\
-                          | |   | |_| | | |__    | | | | | |_/ /   | |   | | | | | |__   | |_/ /
-                          | |   |  _  | |  __|   | | | | |    /    | |   | | | | |  __|  |    / 
-                          | |   | | | | | |____  | |/ /  | |\ \   _| |_  \ \_/ / | |___  | |\ \ 
-                          \_/   \_| |_/ \____(_) |___/   \_| \_|  \___/   \___/  \____/  \_| \_|
-
-
-                                                 ▐▀▀▀▀▀▀▀▀▀▀▀▌
-                                               ▄▄▌░░░░░░░░░░░▐▄▄
-                                              ▄▄██▄▄▄▄▄▄▄▄▄▄▄██▄▄
-                                             ▐█▄█┼┼█▓█▓█▓█▓█┼┼█▄█▌
-                                            ─█▓▓█▀███_6969_███▀█▓▓─
-                                            ─█▓▓█▀▀───▀▀▀▀──▀▀█▓▓█─
-                                            ─█▓▓█─────────────█▓▓█─
-
-''')
-
-print('''
-
-        INSTRUCTIONS
-        ────────────
-        Type 'start' to start the car.
-        Type 'stop' to stop the car.
-        Type 'quit' to quit.
-
-''')
-
-car = {
-    'is_running': False,
-}
-
-main()
+if __name__ == '__main__':
+    main()
